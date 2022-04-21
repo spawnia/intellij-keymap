5c62e31b8910076383b9f274ab0b2b1b9b243bab
spawnia <benedikt@franke.tech>
2022-04-21 16:47:31 +0200

    Update for 2022.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,25 @@
-fun properties(key: String) = project.findProperty(key).toString()
-
 plugins {
     // https://plugins.gradle.org/plugin/org.jetbrains.intellij
-    id("org.jetbrains.intellij") version "1.3.0"
+    id("org.jetbrains.intellij") version "1.5.3"
 }
 
 repositories {
     mavenCentral()
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin#intellij-platform-properties
+// https://github.com/JetBrains/gradle-intellij-plugin#intellij-platform-properties
 intellij {
-    version.set("2021.3")
+    version.set("2022.1")
 }
 
 tasks {
     // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#intellij-platform-based-products-of-recent-ide-versions
     patchPluginXml {
-        version.set("4.4")
+        version.set("4.5")
         sinceBuild.set("211")
     }
 
     publishPlugin {
-        token.set(properties("intellijPublishToken"))
+        token.set(project.findProperty("intellijPublishToken").toString())
     }
 }
