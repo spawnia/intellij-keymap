760588a2dbb98b36b846746718b823d5bcf262b6
spawnia <benedikt@franke.tech>
2022-12-14 15:06:23 +0100

    4.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // https://plugins.gradle.org/plugin/org.jetbrains.intellij
-    id("org.jetbrains.intellij") version "1.7.0"
+    id("org.jetbrains.intellij") version "1.10.1"
 }
 
 repositories {
@@ -9,13 +9,13 @@ repositories {
 
 // https://github.com/JetBrains/gradle-intellij-plugin#intellij-platform-properties
 intellij {
-    version.set("2022.2")
+    version.set("2022.3")
 }
 
 tasks {
     // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#intellij-platform-based-products-of-recent-ide-versions
     patchPluginXml {
-        version.set("4.6")
+        version.set("4.7")
         sinceBuild.set("211")
     }
 
