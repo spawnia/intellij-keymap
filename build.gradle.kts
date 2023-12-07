7f724197c1d4d2d7d86e76e381f1ec19c485c903
spawnia <benedikt@franke.tech>
2023-12-07 09:25:58 +0100

    Update for 2023.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // https://plugins.gradle.org/plugin/org.jetbrains.intellij
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.16.1"
 }
 
 repositories {
@@ -8,15 +8,15 @@ repositories {
 }
 
 intellij {
-    // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#intellij-platform-based-products-of-recent-ide-versions
-    version.set("2023.2")
+    // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#platformVersions
+    version.set("2023.3")
 }
 
 tasks {
-    // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#intellij-platform-based-products-of-recent-ide-versions
+    // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#platformVersions
     patchPluginXml {
-        version.set("4.9")
-        sinceBuild.set("232")
+        version.set("4.10")
+        sinceBuild.set("233")
     }
 
     publishPlugin {
